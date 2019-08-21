a5a30ceb0c5f2d2cbfe22b87f9cab407dff2459f
Artemiy <rendrom@gmail.com>
2019-08-21 14:57:29 +0800

    Header image

--- conferences/petro/index.js
+++ conferences/petro/index.js
@@ -10,14 +10,14 @@ import { createMap } from '../../src/ngwMap';
 onDomLoaded(function () {
   // var url = 'http://127.0.0.1:5500/conferences/petro/data/header.jpg';
   var url = 'https://raw.githubusercontent.com/rendrom/petro2020/master/conferences/petro/data/header.jpg'
-  var hero = document.getElementsByClassName('hero-before')[0];
+  var hero = document.getElementsByClassName('hero')[0];
   var img = new Image();
   img.src = url;
   // Once image is loaded replace the src of the HTML element
   img.onload = function () {
     // var background = "linear-gradient(to top right, #524ad0 10%, #D099FA), url('" + url + "')";
-    var background = "url('" + url + "') center center";
-    hero.style.background = background;
+    var background = "url('" + url + "') ";
+    hero.style.backgroundImage = background;
   };
 
 });
