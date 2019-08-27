2c4c2945eff5500a5478216df10bfa5da9395b0a
Artemiy <rendrom@gmail.com>
2019-08-27 11:20:36 +0800

    Calendar tooltip

--- conferences/petro/createPetroCalendar.js
+++ conferences/petro/createPetroCalendar.js
@@ -7,40 +7,49 @@ export function createPetroCalendar() {
       id: 1,
       name: 'Регистрация участников',
       startDate: new Date(),
-      endDate: new Date(2020, 2, 3)
+      endDate: new Date(2020, 2, 3),
+      color: 'yellow'
     },
     {
       id: 2,
       name: 'Прием тезисов докладов',
-
       startDate: new Date(),
-      endDate: new Date(2020, 5, 30)
+      endDate: new Date(2020, 5, 30),
+      color: 'lightgreen'
     },
     {
       id: 0,
       name: '1-й Циркуляр',
       startDate: new Date(2019, 9, 22),
       endDate: new Date(2019, 9, 22),
+      color: 'red'
     },
     {
       id: 3,
       name: '2-й Циркуляр (программа)',
-      location: 'San Francisco, CA',
       startDate: new Date(2020, 7, 20),
       endDate: new Date(2020, 7, 20),
+      color: 'red'
     },
     {
       id: 4,
       name: 'Совещание',
       startDate: new Date(2020, 9, 7),
-      endDate: new Date(2020, 9, 10)
+      endDate: new Date(2020, 9, 10),
+      color: '#524ad0'
     },
     {
       id: 5,
       name: 'Экскурсии',
       startDate: new Date(2020, 9, 11),
-      endDate: new Date(2020, 9, 13)
+      endDate: new Date(2020, 9, 13),
+      color: '#00d1b2'
     }
   ]
-  createCalendar({ dataSource: dataSource, minDate: new Date(), maxDate: new Date(2020, 9, 13)});
+  createCalendar({
+    dataSource: dataSource,
+    minDate: new Date(),
+    maxDate: new Date(2020, 9, 13),
+    style: 'border'
+  });
 }
--- src/calendar.js
+++ src/calendar.js
@@ -5,14 +5,18 @@ export function createCalendar(options, onCreate) {
 
 
   loadJs('https://unpkg.com/js-year-calendar@latest/dist/js-year-calendar.min.js');
-
   loadCss('https://unpkg.com/js-year-calendar@latest/dist/js-year-calendar.min.css');
+
+  loadJs('https://unpkg.com/popper.js@1');
 
   var intervalId = setInterval(function () {
     if (window.Calendar) {
       loadJs('https://unpkg.com/js-year-calendar@latest/locales/js-year-calendar.ru.js');
     }
-    if (window.Calendar && window.Calendar.locales.ru) {
+    if (window.Popper) {
+      loadJs('https://unpkg.com/tippy.js@4');
+    }
+    if (window.Calendar && window.Calendar.locales.ru && window.Popper) {
       _init(options, onCreate);
       clearInterval(intervalId);
     }
@@ -23,6 +27,32 @@ function _init(options, onCreate) {
   var elem = document.getElementById('extra');
   options = options || {};
   options.language = 'ru';
+
+  options.mouseOnDay = options.mouseOnDay || function (e) {
+    if (e.events.length > 0) {
+      var content = '';
+
+      for (var i in e.events) {
+        content += '<div class="event-tooltip-content">'
+          + '<div class="event-name" style="color:' + e.events[i].color + '">' + e.events[i].name + '</div>'
+          + '</div>';
+      }
+      var tooltip = window.tippy(e.element);
+      if (tooltip) {
+        tooltip.set({
+          content: content,
+          arrow: true,
+          followCursor: true,
+          theme: 'light',
+          animateFill: false,
+          duration: 0,
+          delay: [0, 10]
+        });
+        tooltip.show();
+      }
+    }
+  };
+
   var calendar = new window.Calendar(elem, options)
   if (onCreate) {
     onCreate(calendar);
