c7cd0bf27cd570048464f95bada61b255de5c7b6
Artemiy <rendrom@gmail.com>
2021-01-18 08:44:23 +0800

    feat(petro): update calendar

--- conferences/petro/createPetroCalendar.js
+++ conferences/petro/createPetroCalendar.js
@@ -22,12 +22,9 @@ var messages = {
     conference: 'Совещание',
     excursions: 'Экскурсии'
   }
-}
-
-
+};
 
 export function createPetroCalendar() {
-
   var msg = messages[detectLanguage()];
 
   var dataSource = [
@@ -35,63 +32,63 @@ export function createPetroCalendar() {
       id: 1,
       name: msg.registration,
       startDate: new Date(),
-      endDate: new Date(2020, 2, 16),
+      endDate: new Date(2020, 2, 12),
       color: 'yellow'
     },
     {
       id: 2,
       name: msg.abstracts,
       startDate: new Date(),
-      endDate: new Date(2020, 3, 30),
+      endDate: new Date(2020, 6, 30),
       color: 'lightgreen'
     },
     {
       id: 6,
       name: msg.excursions_registration,
       startDate: new Date(),
-      endDate: new Date(2020, 3, 30),
+      endDate: new Date(2020, 6, 30),
       color: '#00d1b2'
     },
-    {
-      id: 0,
-      name: msg.circular1,
-      startDate: new Date(2019, 9, 1),
-      endDate: new Date(2019, 8, 22),
-      color: 'red'
-    },
+    // {
+    //   id: 0,
+    //   name: msg.circular1,
+    //   startDate: new Date(2019, 9, 1),
+    //   endDate: new Date(2019, 8, 22),
+    //   color: 'red'
+    // },
     {
       id: 3,
       name: msg.circular2,
-      startDate: new Date(2020, 1, 17),
-      endDate: new Date(2020, 1, 17),
+      startDate: new Date(2020, 5, 5),
+      endDate: new Date(2020, 5, 5),
       color: 'red'
     },
     {
       id: 3,
       name: msg.circular3,
-      startDate: new Date(2020, 6, 20),
-      endDate: new Date(2020, 6, 20),
+      startDate: new Date(2020, 5, 21),
+      endDate: new Date(2020, 5, 21),
       color: 'red'
     },
     {
       id: 4,
       name: msg.conference,
-      startDate: new Date(2020, 8, 7),
-      endDate: new Date(2020, 8, 10),
+      startDate: new Date(2020, 8, 6),
+      endDate: new Date(2020, 8, 12),
       color: '#524ad0'
     },
     {
       id: 5,
       name: msg.excursions,
-      startDate: new Date(2020, 8, 11),
-      endDate: new Date(2020, 8, 14),
+      startDate: new Date(2020, 8, 10),
+      endDate: new Date(2020, 8, 12),
       color: '#00d1b2'
     }
   ];
 
   createCalendar({
     dataSource: dataSource,
-    minDate: new Date(new Date().setDate(new Date().getDate()-1)),
+    minDate: new Date(new Date().setDate(new Date().getDate() - 1)),
     maxDate: new Date(2020, 8, 14),
     style: 'border'
   });
