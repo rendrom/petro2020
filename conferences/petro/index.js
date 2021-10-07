153e3d35d9e54e21a93ee3698b99ecbdbcbf5dbe
Artemiy <rendrom@gmail.com>
2021-10-07 18:13:43 +0200

    feat: collective photo lazy load IV

--- conferences/petro/index.js
+++ conferences/petro/index.js
@@ -32,7 +32,7 @@ onDomLoaded(function () {
 });
 
 onDomLoaded(function () {
-  var collectivePhoto = document.getElementsByClassName('collective-photo')[0];
+  var collectivePhoto = document.getElementById('collective-photo');
   if (collectivePhoto) {
     var url = 'http://conf.ict.nsc.ru/files/conferences/petro2020/669776/_DSC5815_1_low.jpg';
     var img = new Image();
